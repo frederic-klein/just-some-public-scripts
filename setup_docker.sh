ce654ce61c959f73f3cc37cfa64b43225ad68f7c
Frederic Klein <klein@modell-aachen.de>
2022-01-17 13:15:16 +0100

    auto yes

--- setup_docker.sh
+++ setup_docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get install ca-certificates curl gnupg lsb-release
+sudo apt-get install -y ca-certificates curl gnupg lsb-release
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
 echo \
@@ -8,4 +8,4 @@ echo \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
