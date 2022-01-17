14505446d66f539890e0ccdfb3b3ff93057bfd45
Frederic Klein <klein@modell-aachen.de>
2022-01-17 18:27:00 +0100

    cleanup before setup

--- setup.sh
+++ setup.sh
@@ -2,7 +2,9 @@
 
 PM4PY_NAME='pm4py_docker'
 WORKING_DIR="$1"
-sudo docker network create --driver bridge common
+sudo docker network rm common;
+sudo docker network create --driver bridge common;
 sudo docker pull pm4py/pm4py-core:latest;
-sudo docker run -d -t --network common --name ${PM4PY_NAME} --mount type=bind,source=${WORKING_DIR},target=/promidigit pm4py/pm4py-core:latest
-sudo docker exec ${PM4PY_NAME} bash -c 'apt-get install -y graphviz libgraphviz-dev pkg-config python3-pip; pip install pygraphviz;'
+sudo docker rm ${PM4PY_NAME};
+sudo docker run -d -t --network common --name ${PM4PY_NAME} --mount type=bind,source=${WORKING_DIR},target=/promidigit pm4py/pm4py-core:latest;
+sudo docker exec ${PM4PY_NAME} bash -c 'apt-get install -y graphviz libgraphviz-dev pkg-config python3-pip; pip install pygraphviz;';
