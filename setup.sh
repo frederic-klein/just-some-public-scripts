47d91e2e5fa04eb3303bb370997dc0bf1f23443a
Frederic Klein <klein@modell-aachen.de>
2022-01-17 13:42:04 +0100

    sudo docker

--- setup.sh
+++ setup.sh
@@ -3,6 +3,6 @@
 PM4PY_NAME='pm4py_docker'
 WORKING_DIR="$1"
 
-docker pull pm4py/pm4py-core:latest;
-docker run -d -t --name ${PM4PY_NAME} --mount type=bind,source=${WORKING_DIR},target=/promidigit pm4py/pm4py-core:latest
-docker exec ${PM4PY_NAME} bash -c 'apt-get install -y graphviz libgraphviz-dev pkg-config python3-pip; pip install pygraphviz;'
+sudo docker pull pm4py/pm4py-core:latest;
+sudo docker run -d -t --name ${PM4PY_NAME} --mount type=bind,source=${WORKING_DIR},target=/promidigit pm4py/pm4py-core:latest
+sudo docker exec ${PM4PY_NAME} bash -c 'apt-get install -y graphviz libgraphviz-dev pkg-config python3-pip; pip install pygraphviz;'
